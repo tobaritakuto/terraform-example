b37b9fd357b9e9ed252073b14d89c4fb754a54f2
tobaritakuto <tobaritakuto19930825@icloud.com>
2021-02-23 08:10:45 +0900

    modify public key name

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,14 +6,14 @@ resource "aws_instance" "default" {
   ami                    = "ami-0992fc94ca0f1415a" #Amazon Linux2
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.default.id]
-  key_name               = aws_key_pair.default.id
+  key_name               = aws_key_pair.tobari.id
 
   tags = {
     Name = "tobari_ec2"    
   }
 }
 
-resource "aws_key_pair" "default" {
-  key_name   = "example"
+resource "aws_key_pair" "tobari" {
+  key_name   = "tobari_public_key"
   public_key = file("${path.module}/key.pub")
 }
